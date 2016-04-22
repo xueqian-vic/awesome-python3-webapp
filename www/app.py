249c9ff139b2287a87e7c5b25fd1a68133b2ad67
xueqian-vic <xueqian_victoria@163.com>
2016-04-22 11:12:14 +0800

    搭建web app骨架

--- www/app.py
+++ www/app.py
@@ -4,14 +4,14 @@ import asyncio,time,os,json
 
 
 def index(request):
-    return web.Response(b'<h1>index</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_router('GET','/',index)
+    app.router.add_route('GET','/',index)
     svr = yield from loop.create_server(app.make_handler(),'127.0.0.1',8088)
-    logging.info('server started at http://127.0.0.1:9000...')
+    logging.info('server started at http://127.0.0.1:8088...')
     return svr
 
 loop = asyncio.get_event_loop()
